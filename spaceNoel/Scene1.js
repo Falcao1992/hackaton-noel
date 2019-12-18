5d548a98a8c385865bd2b5ef74ac231c23af3f37
Falcao1992 <eduardo.marselle@gmail.com>
2019-12-18 19:10:01 +0100

    fonctionelle

--- spaceNoel/Scene1.js
+++ spaceNoel/Scene1.js
@@ -3,40 +3,45 @@ class Scene1 extends Phaser.Scene {
     super("bootGame");
   }
 
-  preload() {
+  preload(){
     this.load.image("background", "assets/images/background.png");
     //
-    this.load.spritesheet("ship", "assets/spritesheets/ship.png", {
+    this.load.spritesheet("ship", "assets/spritesheets/ship.png",{
       frameWidth: 16,
       frameHeight: 16
     });
-    this.load.spritesheet("ship2", "assets/spritesheets/ship2.png", {
+    this.load.spritesheet("ship2", "assets/spritesheets/ship2.png",{
       frameWidth: 32,
       frameHeight: 16
     });
-    this.load.spritesheet("ship3", "assets/spritesheets/ship3.png", {
+    this.load.spritesheet("ship3", "assets/spritesheets/ship3.png",{
       frameWidth: 32,
       frameHeight: 32
     });
-    this.load.spritesheet("explosion", "assets/spritesheets/explosion.png", {
+    this.load.spritesheet("explosion", "assets/spritesheets/explosion.png",{
       frameWidth: 16,
       frameHeight: 16
     });
-    // 2.1 load the spritesheet
-    this.load.spritesheet("power-up", "assets/spritesheets/power-up.png", {
+    this.load.spritesheet("power-up", "assets/spritesheets/power-up.png",{
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("player", "assets/spritesheets/player.png",{
+      frameWidth: 16,
+      frameHeight: 24
+    });
+    this.load.spritesheet("beam", "assets/spritesheets/beam.png",{
       frameWidth: 16,
       frameHeight: 16
     });
 
-    this.load.spritesheet("player", "assets/spritesheets/player.png", {
-      frameWidth: 16,
-      frameHeight: 24
-    });
+    this.load.bitmapFont("pixelFont", "assets/font/font.png", "assets/font/font.xml");
   }
 
   create() {
     this.add.text(20, 20, "Loading game...");
     this.scene.start("playGame");
+
     this.anims.create({
       key: "ship1_anim",
       frames: this.anims.generateFrameNumbers("ship"),
@@ -64,7 +69,6 @@ class Scene1 extends Phaser.Scene {
       hideOnComplete: true
     });
 
-    //2.1 Two Animations for the power ups
     this.anims.create({
       key: "red",
       frames: this.anims.generateFrameNumbers("power-up", {
@@ -74,7 +78,6 @@ class Scene1 extends Phaser.Scene {
       frameRate: 20,
       repeat: -1
     });
-
     this.anims.create({
       key: "gray",
       frames: this.anims.generateFrameNumbers("power-up", {
@@ -84,15 +87,19 @@ class Scene1 extends Phaser.Scene {
       frameRate: 20,
       repeat: -1
     });
+    this.anims.create({
+      key: "thrust",
+      frames: this.anims.generateFrameNumbers("player"),
+      frameRate: 20,
+      repeat: -1
+    });
 
-    this.anims.create(
-      {
-        key: "thrust",
-        frames: this.anims.generateFrameNumbers("player"),
-        frameRate: 20,
-        repeat: -1
+    this.anims.create({
+      key: "beam_anim",
+      frames: this.anims.generateFrameNumbers("beam"),
+      frameRate: 20,
+      repeat: -1
+    });
 
-      }
-    )
   }
 }
--- spaceNoel/game.js
+++ spaceNoel/game.js
@@ -1,20 +1,23 @@
 var gameSettings = {
-  playerSpeed: 200
+  playerSpeed: 200,
+  maxPowerups: 2,
+  powerUpVel: 50,
 }
 
 var config = {
-  width: 700,
-  height: 500,
+  width: 256,
+  height: 272,
   backgroundColor: 0x000000,
   scene: [Scene1, Scene2],
   pixelArt: true,
-  // 1.1 set the physics to arcade
   physics: {
     default: "arcade",
     arcade:{
-        debug: false
+        debug: false,
+        debugShowVelocity: false
     }
   }
 }
+
 
 var game = new Phaser.Game(config);
